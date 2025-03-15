b7f878515186234e293dcba55838752c3bed622c
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-03-15 08:53:46 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import Navbar from './Pages/Navbar'
 import Sports from './Pages/Sports'
 import Home from './Pages/Home'
-import Dancer from './Pages/Dancer'
-import Singer from './Pages/Singer'
-import Cricket from './Pages/Cricket'
-import Hockey from './Pages/Hockey'
+import Science from './Pages/Science'
+import Technology from './Pages/Technology'
+import Health from './Pages/Health'
+import Business from './Pages/Business'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default class App extends Component {
@@ -23,19 +23,19 @@ export default class App extends Component {
         </Routes>
 
         <Routes>
-          <Route path="/category/health" element={<Cricket purl={"health"} />} />
+          <Route path="/category/health" element={<Health purl={"health"} />} />
         </Routes>
 
         <Routes>
-          <Route path="/category/business" element={<Hockey purl={"business"} />} />
+          <Route path="/category/business" element={<Business purl={"business"} />} />
         </Routes>
 
         <Routes>
-          <Route path="/category/science" element={<Dancer purl={"science"} />} />
+          <Route path="/category/science" element={<Science purl={"science"} />} />
         </Routes>
 
         <Routes>
-          <Route path="/category/technology" element={<Singer purl={"technology"} />} />
+          <Route path="/category/technology" element={<Technology purl={"technology"} />} />
         </Routes>
 
       </BrowserRouter>
