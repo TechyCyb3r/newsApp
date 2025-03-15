6a5dd467f3665bc6c7526a4788fb1c839e089b63
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-03-15 10:56:45 +0530

    changes

--- src/Pages/NewsItem.jsx
+++ src/Pages/NewsItem.jsx
@@ -5,15 +5,18 @@ export class NewsItem extends Component {
     let { title, description, imgUrl, newsUrl } = this.props;
     return (
       <>
-        <div className="card" style={{ width: "19rem, height:40px", marginTop: "20px", alignContent: "space-between", alignItems: "center", justifyContent: "center", display:"grid", flexWrap:"wrap"}}>
+        <div className="card d-flex flex-column" style={{ width: "19rem, height:40px", marginTop: "20px", alignContent: "space-between", alignItems: "center", justifyContent: "center", display:"grid", flexWrap:"wrap"}}>
+
           <div style={{ width: "100%", maxHeight: "165px", height: "165px", overflow: "hidden", display: "grid", alignContent: "center", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
             <img src={imgUrl} className="card-img-top" alt="..." />
           </div>
-          <div className="card-body d-flex flex-column" style={{ display: "grid", alignContent: "space-between", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
+
+          <div className="card-body" style={{ display: "grid", alignContent: "space-between", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
             <a href={newsUrl} target="_black" className="btn btn-sm btn-dark w-50">Read More</a>
           </div>
+
         </div>
       </>
     )
