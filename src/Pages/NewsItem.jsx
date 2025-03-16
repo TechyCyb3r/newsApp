acc077d04212863d64299ee151709fc1886450c9
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-03-16 09:30:16 +0530

    changes

--- src/Pages/NewsItem.jsx
+++ src/Pages/NewsItem.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
-import Styles from '../Styles/NewsItem.module.css'
+import React, { Component } from 'react';
+import Styles from '../Styles/NewsItem.module.css';
 
 export class NewsItem extends Component {
   render() {
     let { title, description, imgUrl, newsUrl } = this.props;
     return (
       <>
-        <div className={`card ${Styles.fcard}` } style={{ width: "19rem, height:40px", marginTop: "20px", alignContent: "space-between", alignItems: "center", justifyContent: "center"}}>
+        <div className={`card ${Styles.fcard}`}>
 
-          <div style={{ width: "100%", height: "165px", overflow: "hidden", alignContent: "center", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
-            <img src={imgUrl} className="card-img-top" alt="..." />
+          <div style={{ width: "100%", height: "165px", overflow: "hidden", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <img src={imgUrl} className="card-img-top" alt="News" />
           </div>
 
-          <div className="card-body" style={{ display: "grid", alignContent: "space-between", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
+          <div className="card-body" style={{ textAlign: "center" }}>
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_black" className="btn btn-sm btn-dark w-50">Read More</a>
+            <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark w-50">Read More</a>
           </div>
 
         </div>
       </>
-    )
+    );
   }
 }
 
-export default NewsItem        
+export default NewsItem;
