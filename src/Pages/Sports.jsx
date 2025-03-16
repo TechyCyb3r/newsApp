42f41e6286a704a52ca4451627098c0a364af84f
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-03-16 09:52:33 +0530

    changes

--- src/Pages/Sports.jsx
+++ src/Pages/Sports.jsx
@@ -67,41 +67,49 @@ export class News extends Component {
 
   render() {
     return (
-      <div className="container text-center my-3" style={{marginTop: "75px"}}>
-        <h1 className="headingTitle" style={{marginTop:"80px"}}>MyNewsApp - Top news Headlines</h1>
+      <div className="container-fluid text-center my-3" style={{ marginTop: "75px" }}>
+        <h1 className="headingTitle h2 h-md-1" style={{ marginTop: "80px" }}>MyNewsApp - Top News Headlines</h1>
         <div className="row my-3">
           {
             this.state.loading ? (
-            <div class="d-flex justify-content-center">
-              <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-              </div>
-            </div>
-            ) :
-            
-              this.state.articles && this.state.articles.map((element) => {
-                return <div className="col-md-4 my-3" key={element.url}>
-                  <NewsItem title={(element.title.length > 45) ? element.title.slice(0, 39) : element.title}
-                    description={
-                      element.description === null
-                        ? "No description available"
-                        : (element.description.length > 30
-                          ? element.description.slice(0, 39)
-                          : element.description)
-                    }
-                    imgUrl={element.urlToImage} newsUrl={element.url} />
+              <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "50vh" }}>
+                <div className="spinner-border" role="status">
+                  <span className="visually-hidden">Loading...</span>
                 </div>
+              </div>
+            ) :
+              this.state.articles && this.state.articles.map((element) => {
+                return (
+                  <div className="col-12 col-sm-6 col-md-4 my-3" key={element.url}>
+                    <NewsItem
+                      title={(element.title.length > 45) ? element.title.slice(0, 39) : element.title}
+                      description={
+                        element.description === null
+                          ? "No description available"
+                          : (element.description.length > 30
+                            ? element.description.slice(0, 39)
+                            : element.description)
+                      }
+                      imgUrl={element.urlToImage}
+                      newsUrl={element.url}
+                    />
+                  </div>
+                );
               })
           }
-
-          {/* //If want to try to add next and previous button
-          // <div className="container d-flex justify-content-between">
-          //   <button type="button" disabled={this.state.page <= 1} onClick={this.previous} className="btn btn-dark">&larr; Previous</button>
-          //   <button type="button" onClick={this.next} className="btn btn-dark">&rarr; Next</button>
-          // </div> */}
-
         </div>
-      </div>
+
+        {/* // If want to try to add next and previous button */}
+        {/* <div className="container-fluid d-flex justify-content-between">
+          <button type="button" disabled={this.state.page <= 1} onClick={this.previous} className="btn btn-dark btn-sm">
+            &larr; Previous
+          </button>
+          <button type="button" onClick={this.next} className="btn btn-dark btn-sm">
+            Next &rarr;
+          </button>
+        </div>
+      </div> */}
+
     )
   }
 }
