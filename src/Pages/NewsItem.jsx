370aba732d6b9a331fe21d0a73b5475c41618a01
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-03-16 09:21:49 +0530

    changes

--- src/Pages/NewsItem.jsx
+++ src/Pages/NewsItem.jsx
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
       <>
         <div className={`card` } style={{ width: "19rem, height:40px", marginTop: "20px", alignContent: "space-between", alignItems: "center", justifyContent: "center"}}>
 
-          <div style={{ width: "100rem", height: "165px", overflow: "hidden", alignContent: "center", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
+          <div style={{ width: "100%", height: "165px", overflow: "hidden", alignContent: "center", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
             <img src={imgUrl} className="card-img-top" alt="..." />
           </div>
 
-          <div className="card-body" style={{ display: "flex", alignContent: "space-between", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
+          <div className="card-body" style={{ display: "grid", alignContent: "space-between", alignItems: "center", justifyItems: "center", justifyContent: "center" }}>
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
             <a href={newsUrl} target="_black" className="btn btn-sm btn-dark w-50">Read More</a>
