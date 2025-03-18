4d907d188ed76e6309576f26ffb35c3f9c3a905a
Himanshu Agarwal <134074266+TechyCyb3r@users.noreply.github.com>
2025-03-18 19:55:00 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './Pages/Navbar'
 import News from './Pages/News'
-// import Home from './Pages/Home'
-// import Science from './Pages/Science'
-// import Technology from './Pages/Technology'
-// import Health from './Pages/Health'
-// import Business from './Pages/Business'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default class App extends Component {
@@ -14,28 +9,28 @@ export default class App extends Component {
       <BrowserRouter>
         <><Navbar /></>
 
-        <Routes>  
+        <Routes>
           <Route path="/" element={<News purl={"general"} headline={"News Headlines"} />} />
         </Routes>
 
         <Routes>
-          <Route path="/category/sports" element={<News purl={"sports"}  headline={"Sports Headlines"}/>} />
+          <Route path="/category/sports" element={<News purl={"sports"} headline={"Sports Headlines"} />} />
         </Routes>
 
         <Routes>
-          <Route path="/category/health" element={<News purl={"health"}  headline={"Health Headlines"}/>} />
+          <Route path="/category/health" element={<News purl={"health"} headline={"Health Headlines"} />} />
         </Routes>
 
         <Routes>
-          <Route path="/category/business" element={<News purl={"business"}  headline={"Business Headlines"}/>} />
+          <Route path="/category/business" element={<News purl={"business"} headline={"Business Headlines"} />} />
         </Routes>
 
         <Routes>
-          <Route path="/category/science" element={<News purl={"science"}  headline={"Science Headlines"}/>} />
+          <Route path="/category/science" element={<News purl={"science"} headline={"Science Headlines"} />} />
         </Routes>
 
         <Routes>
-          <Route path="/category/technology" element={<News purl={"technology"}  headline={"Technology Headlines"}/>} />
+          <Route path="/category/technology" element={<News purl={"technology"} headline={"Technology Headlines"} />} />
         </Routes>
 
       </BrowserRouter>
